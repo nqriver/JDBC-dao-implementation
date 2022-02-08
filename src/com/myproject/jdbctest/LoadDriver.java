47de15769c94a157117f5334adaf8e239f1c8bff
nqriver <nqriver@gmail.com>
2022-02-09 00:29:43 +0100

    update query tested

--- src/com/myproject/jdbctest/LoadDriver.java
+++ src/com/myproject/jdbctest/LoadDriver.java
@@ -4,22 +4,31 @@ import java.sql.*;
 
 
 public class LoadDriver {
+
+    private static final String sqlSelectAllPersons = "SELECT * FROM employees";
+    private static final String setJohnAsJohnny = "UPDATE EMPLOYEES SET FIRST_NAME = ? WHERE FIRST_NAME = ?";
+    private static final String dbUrl = "jdbc:mysql://localhost:3306/demo?serverTimezone=UTC";
+    private static final String password = "lukas";
+    private static final String username = "lukas";
+
     public static void main(String[] args) {
-
-        final String sqlSelectAllPersons = "SELECT * FROM employees";
-        final String connectionUrl = "jdbc:mysql://localhost:3306/demo?serverTimezone=UTC";
-
         try {
-            Connection conn = DriverManager.getConnection(connectionUrl, "lukas", "lukas");
+            Connection conn = DriverManager.getConnection(dbUrl, username, password);
+            PreparedStatement updateStatement = conn.prepareStatement(setJohnAsJohnny);
+            updateStatement.setString(1, "Johnny");
+            updateStatement.setString(2, "John");
+
+            updateStatement.executeUpdate();
+
             PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
             ResultSet rs = ps.executeQuery();
 
-                while (rs.next()) {
-                    long id = rs.getLong("ID");
-                    String lastName = rs.getString("LAST_NAME");
-                    String first_name = rs.getString("FIRST_NAME");
-                    String email = rs.getString("EMAIL");
-                    String department = rs.getString("DEPARTMENT");
+            while (rs.next()) {
+                long id = rs.getLong("ID");
+                String lastName = rs.getString("LAST_NAME");
+                String first_name = rs.getString("FIRST_NAME");
+                String email = rs.getString("EMAIL");
+                String department = rs.getString("DEPARTMENT");
                     double salary = rs.getDouble("SALARY");
 
                     System.out.printf("We have got you: %s %s\n", first_name, lastName);
